0b88d1fe81930ce18e03c53bcaf2b5165e3cffca
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 12:43:39 +0800

    Pretty printed the Arduino code with astyle

--- Grove_I2C_Motor_Driver.h
+++ Grove_I2C_Motor_Driver.h
@@ -1,33 +1,33 @@
-/* 
- * Grove_I2C_Motor_Driver.h
- * A library for Grove - I2C Motor Driver v1.3
- *
- * Copyright (c) 2012 seeed technology inc.
- * Website    : www.seeed.cc
- * Author     : Jerry Yip
- * Create Time: 2017-02
- * Change Log :
- *
- * The MIT License (MIT)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+/*
+    Grove_I2C_Motor_Driver.h
+    A library for Grove - I2C Motor Driver v1.3
+
+    Copyright (c) 2012 seeed technology inc.
+    Website    : www.seeed.cc
+    Author     : Jerry Yip
+    Create Time: 2017-02
+    Change Log :
+
+    The MIT License (MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
 
 #ifndef __I2CMOTORDRIVER_H__
 #define __I2CMOTORDRIVER_H__
@@ -60,10 +60,9 @@
 #define F_30Hz                    0x05
 
 /*************************Class for Grove I2C Motor Driver********************/
-class I2CMotorDriver
-{
+class I2CMotorDriver {
 
-private:
+  private:
     // _speed0: 0~100  _speed1: 0~100
     unsigned char _speed1 = 0;
     unsigned char _speed2 = 0;
@@ -77,13 +76,13 @@ private:
     void direction(unsigned char _direction);
     unsigned char _step_cnt = 0;
 
-public:
+  public:
     // Initialize I2C with an I2C address you set on Grove - I2C Motor Driver v1.3
     // default i2c address: 0x0f
     int begin(unsigned char i2c_add);
     // Set the speed of a motor, speed is equal to duty cycle here
     // motor_id: MOTOR1, MOTOR2
-    // _speed: -100~100, when _speed>0, dc motor runs clockwise; 
+    // _speed: -100~100, when _speed>0, dc motor runs clockwise;
     // when _speed<0, dc motor runs anticlockwise
     void speed(unsigned char motor_id, int _speed);
     // Set the frequence of PWM(cycle length = 510, system clock = 16MHz)
@@ -94,7 +93,7 @@ public:
     // motor_id: MOTOR1, MOTOR2
     void stop(unsigned char motor_id);
     // Drive a stepper motor
-    // _step: -1024~1024, when _step>0, stepper motor runs clockwise; when 
+    // _step: -1024~1024, when _step>0, stepper motor runs clockwise; when
     // _step<0, stepper motor runs anticlockwise; when _step is 512, the stepper
     // motor will run a complete turn; if step is 1024, the stepper motor will
     // run 2 turns.
@@ -102,7 +101,7 @@ public:
     //        1 -> 2 phase stepper motor
     //  _mode: 0 -> compatible mode (_step=1 corresponds 4 steps)
     //         1 -> fine mode (_step1 corresponds 1 steps)
-    void StepperRun(int _step, int _type = 0, int _mode = 0); 
+    void StepperRun(int _step, int _type = 0, int _mode = 0);
 };
 
 extern I2CMotorDriver Motor;
