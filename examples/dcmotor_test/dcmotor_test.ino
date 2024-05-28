4449a9909c5e4daa14802d7971953e31d7b5f529
benppppp <benoit.pierret@insa-lyon.fr>
2024-05-28 15:38:12 +0200

    frequence in example

--- examples/dcmotor_test/dcmotor_test.ino
+++ examples/dcmotor_test/dcmotor_test.ino
@@ -1,33 +1,33 @@
-/*
-    motor_test.ino
-    Example sketch for Grove - I2C Motor Driver v1.3
-
-    Copyright (c) 2012 seeed technology inc.
-    Website    : www.seeed.cc
-    Author     : Jerry Yip
-    Create Time: 2017-02
-    Change Log :
-
-    The MIT License (MIT)
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-*/
+/* 
+ * motor_test.ino
+ * Example sketch for Grove - I2C Motor Driver v1.3
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Jerry Yip, benppppp
+ * Create Time: 2017-02
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "Grove_I2C_Motor_Driver.h"
 
@@ -35,25 +35,28 @@
 #define I2C_ADDRESS 0x0f
 
 void setup() {
-    Serial.begin(9600);
-    Motor.begin(I2C_ADDRESS);
+  Motor.begin(I2C_ADDRESS);
 }
 
 void loop() {
-    // Set speed of MOTOR1, Clockwise, speed: -100~100
-    Motor.speed(MOTOR1, 50);
-    // Set speed of MOTOR2, Anticlockwise
-    Motor.speed(MOTOR2, -70);
-    delay(2000);
-    // Change speed and direction of MOTOR1
-    Motor.speed(MOTOR1, -100);
-    // Change speed and direction of MOTOR2
-    Motor.speed(MOTOR2, 100);
-    delay(2000);
-    // Stop MOTOR1 and MOTOR2
-    Motor.stop(MOTOR1);
-    Motor.stop(MOTOR2);
-    delay(2000);
+  // Set speed of MOTOR1, Clockwise, speed: -255~255 
+  Motor.speed(MOTOR1, 100);
+  // Set speed of MOTOR2, Anticlockwise
+  Motor.speed(MOTOR2, -120);
+  delay(2000);
+  // Change PWM frequence for both motor
+  // Motor.frequence(1000);//for motor driver STM32 (L298) - Possible frequences 1 - > 65535
+  //Motor.frequence(F_490Hz); //for motor driver 1.3 - Possible frequences F_31372Hz, F_3921Hz, F_490Hz, F_122Hz, F_30Hz
+  // Change speed and direction of MOTOR1
+  Motor.speed(MOTOR1, -255);
+  // Change speed and direction of MOTOR2
+  Motor.speed(MOTOR2, 255);
+  delay(2000);
+  // Stop MOTOR1 and MOTOR2
+  // Can be stopped with speed 0 too!
+  Motor.stop(MOTOR1);
+  Motor.stop(MOTOR2);
+  delay(2000);
 }
 
 // End of file
