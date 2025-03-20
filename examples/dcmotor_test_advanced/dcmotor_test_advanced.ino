a1a4284e61ee99eb6dfa696437e86caeb4e72968
unknown <benoit.pierret@insa-lyon.fr>
2025-03-20 11:10:45 +0100

    new examples dcmotor advanced and minimal

--- examples/dcmotor_test_advanced/dcmotor_test_advanced.ino
+++ examples/dcmotor_test_advanced/dcmotor_test_advanced.ino
@@ -36,21 +36,38 @@
 
 void setup() {
   Motor.begin(I2C_ADDRESS);
+  Serial.begin(9600);
+  Serial.println();
+  Motor.timeout(1000); //motor stop after this time in millis - available for firmware v105+
+  Motor.frequence(1000);  //for motor driver STM32 (L298) - Possible frequences 1 - > 65535 - available for firmware v103+
+  //Motor.frequence(F_490Hz); //for motor driver 1.3 - Possible frequences F_31372Hz, F_3921Hz, F_490Hz, F_122Hz, F_30Hz
+  
+  //Get firmware version
+  unsigned int version = Motor.getversion();  //Get the motor driver firmware version - version avaiable for firmware v105+
+  if (version == 0) {
+    Serial.println("Firmware <105 -> upgrade your L298 motor driver firmware !");
+  } else if (version > 0 && version < 6) {
+    Serial.println("Wire I2C error: " + (String)version);
+  } else {
+    Serial.println("Firmware version: " + (String)version);
+  }
 }
 
 void loop() {
-  // Set speed of MOTOR1, Clockwise, speed: -255~255 
+  // Set speed of MOTOR1, Clockwise, speed: -255~255
   Motor.speed(MOTOR1, 100);
   // Set speed of MOTOR2, Anticlockwise
   Motor.speed(MOTOR2, -120);
-  delay(2000);
-  // Change PWM frequence for both motor
-  // Motor.frequence(1000);//for motor driver STM32 (L298) - Possible frequences 1 - > 65535
-  //Motor.frequence(F_490Hz); //for motor driver 1.3 - Possible frequences F_31372Hz, F_3921Hz, F_490Hz, F_122Hz, F_30Hz
+  // Verify timed out before and after delay
+  Serial.print("Timed out1: ");
+  Serial.print(Motor.gettimedout());
+  delay(4000);
+  Serial.print(" Timed out2: ");
+  Serial.println(Motor.gettimedout());
   // Change speed and direction of MOTOR1
-  Motor.speed(MOTOR1, -255);
+  Motor.speed(MOTOR1, -50);
   // Change speed and direction of MOTOR2
-  Motor.speed(MOTOR2, 255);
+  Motor.speed(MOTOR2, 50);
   delay(2000);
   // Stop MOTOR1 and MOTOR2
   // Can be stopped with speed 0 too!
